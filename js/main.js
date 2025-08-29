4a99dd26d0ed5f9041b13f0b2f8954dd4d62df17
lr-491 <lrafiou491@gmail.com>
2025-08-29 03:41:50 +0200

    Update home page and navbar

--- js/main.js
+++ js/main.js
@@ -2,22 +2,39 @@ document.addEventListener("DOMContentLoaded", function() {
   new SimpleLightbox('.gallery a', { captions: false });
 });
 
-// Script for menu button
-  const menuBtn = document.getElementById('menu-btn');
-  const mobileMenu = document.getElementById('mobile-menu');
-  const closeMenu = document.getElementById('close-menu');
+  // Mobile menu toggle
+  const toggleBtn = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
 
-  menuBtn.addEventListener('click', () => {
-    mobileMenu.classList.remove('-translate-x-full');
+  toggleBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
   });
 
-  closeMenu.addEventListener('click', () => {
-    mobileMenu.classList.add('-translate-x-full');
-  });
-
-  // (Optionnel) Ferme menu quand on clique sur un lien
-  document.querySelectorAll('#mobile-menu a').forEach(link => {
-    link.addEventListener('click', () => {
-      mobileMenu.classList.add('-translate-x-full');
+  // Fermer le menu mobile après un clic sur un lien
+  document.querySelectorAll("#mobile-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+      mobileMenu.classList.add("hidden");
     });
+  });
+
+// Marquer le lien actif
+  const currentPage = window.location.pathname.split("/").pop(); // exemple: 'services.html'
+
+  document.querySelectorAll(".nav-link").forEach(link => {
+    const linkHref = link.getAttribute("href").split("/").pop(); // extrait: 'services.html'
+    
+    if (linkHref === currentPage) {
+      link.classList.add(
+        "text-blue-600",
+        "font-semibold",
+        "border-l-4",
+        "border-blue-600",
+        "bg-blue-50",      // petit fond doux bleu
+        "pl-2",             // petit espace entre texte et bordure
+        "rounded-sm",
+        "w-32",
+        "Px-4",
+        "py-2" 
+         );
+    }
   });
